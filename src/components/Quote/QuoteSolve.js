585a9488e3c833e56c1dc9d6e01a3c3aea6244a1
JakeSeib <jacob.a.seib@gmail.com>
2020-03-31 17:18:41 -0400

    Add handleGuess function to QuoteSolve
    QuoteSolve has function for handling a new letter guess
    quoteDisplay returns the set of alphabetical characters in the original
    quote as well as formatted text & author

--- src/components/Quote/QuoteSolve.js
+++ src/components/Quote/QuoteSolve.js
@@ -4,28 +4,61 @@ import quoteDisplay from './quoteDisplay'
 import './QuoteSolve.scss'
 // import messages from '../AutoDismissAlert/messages'
 
-const QuoteSolve = ({ quote, user }) => {
-  const [guess] = useState({
-    text: '',
-    author: ''
-  })
+// convention is that uppercased letters denote encoded letters, lowercased
+// letters denote either the solution text or the user's current guess at the
+// solution text
 
+function GuessHash (charSet) {
+  charSet.forEach(char => {
+    this[char] = null
+  })
+}
+
+const QuoteSolve = ({ quote, user }) => {
   quoteDisplay(quote)
+  const [guessHash, setGuessHash] = useState(new GuessHash(quote.charSet))
+  const [guessQuote, setGuessQuote] = useState({
+    text: quote.formattedText,
+    author: quote.formattedAuthor
+  })
 
   // set up individual elements for each letter
   // those individual elements show the current guess above the formatted letter
   // set event listeners onClick for alphabetical letters
 
-  // store and use guess in similar way as quoteDisplay: make hash of
-  // guessedLetters whose keys are the set of alphabetic characters in the quote
-  // and whose values are the current guesses for those letters (alter
-  // quoteDisplay and createDifficulty to filter out non-alphabetical characters)
-
   // onClick checks the formattedText/Author letter of that element and uses
   // that as the key to update guessedLetters
 
   // when guessedLetters changes, update guess
-  // figure out how to update guess when two guesses are getting swapped
+
+  const handleGuess = (cipherLetter, newGuess) => {
+    newGuess = newGuess.toLowerCase()
+    const oldGuess = guessHash[cipherLetter]
+    const oldCipherPair = Object.entries(guessHash).find(pair => pair[1] === newGuess)
+    const newGuessQuote = { ...guessQuote }
+    // newGuess was previously guessed elsewhere
+    if (oldCipherPair) {
+      // newGuess was previously guessed elsewhere & cipherLetter had a previous guess
+      if (oldGuess) {
+        newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${oldGuess}`, 'g'), cipherLetter)
+        newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${oldGuess}`, 'g'), cipherLetter)
+        newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${newGuess}`, 'g'), oldGuess)
+        newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${newGuess}`, 'g'), oldGuess)
+      } else {
+        newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${newGuess}`, 'g'), oldCipherPair[0])
+        newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${newGuess}`, 'g'), oldCipherPair[0])
+      }
+      // cipherLetter had a previous guess
+    } else if (oldGuess) {
+      newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${oldGuess}`, 'g'), newGuess)
+      newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${oldGuess}`, 'g'), newGuess)
+    }
+    // base case
+    newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${cipherLetter}`, 'g'), newGuess)
+    newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${cipherLetter}`, 'g'), newGuess)
+    setGuessHash((new Set(guessHash))[cipherLetter] = newGuess)
+    setGuessQuote(newGuessQuote)
+  }
 
   const quoteTextJSX =
     quote.formattedText.split(' ').map((word, wordIndex) => (
@@ -39,7 +72,7 @@ const QuoteSolve = ({ quote, user }) => {
   return (
     <section className='solve-puzzle'>
       {quoteTextJSX}
-      <p>{guess.text}</p>
+      <p>{guessQuote.text}</p>
     </section>
   )
 }
--- src/components/Quote/quoteDisplay.js
+++ src/components/Quote/quoteDisplay.js
@@ -1,33 +1,44 @@
 const formatString = function (string, cipher) {
-  // given a string, format for display using the provided cipher
+  // given a string, format for display using the provided cipher and provide
+  // set of alphabetical characters in string
   const alpha = 'abcdefghijklmnopqrstuvwxyz'
   const charSet = new Set(string.toLowerCase())
-  // filter out non-alphabetical characters
-  charSet.forEach(char => {
-    if (alpha.indexOf(char) < 0) {
-      charSet.delete(char)
-    }
-  })
   let formattedString = string.toLowerCase()
-  // take lowercased string, and for each unique character in the string,
-  // replace all instances in the string with the corresponding uppercased
-  // letter from the cipher, leaving non-alphabetical characters in place
+  // filter out non-alphabetical characters
+
+  // for alphabetical characters, replace all instances in the string with the
+  // corresponding uppercased letter from the cipher, leaving non-alphabetical
+  // characters in place
   charSet.forEach(char => {
-    const alphaIndex = [...alpha].findIndex(el => el === char)
-    if (alphaIndex > -1) {
+    const alphaIndex = alpha.indexOf(char)
+    if (alphaIndex < 0) {
+      charSet.delete(char)
+    } else {
       const regex = new RegExp(`${char}`, 'g')
       formattedString = formattedString.replace(regex, [...cipher][alphaIndex].toUpperCase())
     }
   })
-  return formattedString
+
+  return { formattedString, charSet }
+}
+
+function union (setA, setB) {
+  const _union = new Set(setA)
+  for (const elem of setB) {
+    _union.add(elem)
+  }
+  return _union
 }
 
 const quoteDisplay = function (quoteObj) {
   // given a Quote object with text, author, and cipher, adds properties for
   // formatted text and formatted author using the cipher
+  const formattedText = formatString(quoteObj.text, quoteObj.cipher)
+  const formattedAuthor = formatString(quoteObj.author, quoteObj.cipher)
   const formattedQuote = {
-    formattedText: formatString(quoteObj.text, quoteObj.cipher),
-    formattedAuthor: '-' + formatString(quoteObj.author, quoteObj.cipher)
+    formattedText: formattedText.formattedString,
+    formattedAuthor: '-' + formattedAuthor.formattedString,
+    charSet: union(formattedText.charSet, formattedAuthor.charSet)
   }
   return Object.assign(quoteObj, formattedQuote)
 }
