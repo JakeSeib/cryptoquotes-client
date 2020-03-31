ebee1da7bc28fc7e9848b856bd23d4e70578529e
JakeSeib <jacob.a.seib@gmail.com>
2020-03-31 11:29:11 -0400

    Reorder nav dropdown items
    And clean up header component code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,27 +4,12 @@ import Navbar from 'react-bootstrap/Navbar'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 
-// const authenticatedOptions = (
-//   <Fragment>
-//     <Nav.Link href="#change-password">Change Password</Nav.Link>
-//     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-//   </Fragment>
-// )
-
-// const authenticatedOptions = (
-//   <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-//     <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-//     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-//     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-//   </DropdownButton>
-// )
-
 const authenticatedOptions = (
   <DropdownButton id="dropdown-basic-button" variant="secondary" alignRight title="Options">
-    <Dropdown.Item href="#change-password">Change Password</Dropdown.Item>
-    <Dropdown.Item href="#sign-out">Sign Out</Dropdown.Item>
     <Dropdown.Item href="#quotes">Browse Quotes</Dropdown.Item>
     <Dropdown.Item href="#quotes/create">Create a Quote</Dropdown.Item>
+    <Dropdown.Item href="#change-password">Change Password</Dropdown.Item>
+    <Dropdown.Item href="#sign-out">Sign Out</Dropdown.Item>
   </DropdownButton>
 )
 
@@ -34,12 +19,6 @@ const unauthenticatedOptions = (
     <Nav.Link href="#sign-in">Sign In</Nav.Link>
   </Fragment>
 )
-
-// const alwaysOptions = (
-//   <Fragment>
-//     <Nav.Link to="/">Home</Nav.Link>
-//   </Fragment>
-// )
 
 const Header = ({ user }) => (
   <Navbar bg="dark" variant="dark" expand={user ? true : 'md'}>
