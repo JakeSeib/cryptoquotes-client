23e6b7cc2cff3ae0497302cc6e8f22668f230806
JakeSeib <jacob.a.seib@gmail.com>
2020-03-31 17:32:23 -0400

    Update guessHash on guess

--- src/components/Quote/QuoteSolve.js
+++ src/components/Quote/QuoteSolve.js
@@ -4,6 +4,7 @@ import quoteDisplay from './quoteDisplay'
 import './QuoteSolve.scss'
 // import messages from '../AutoDismissAlert/messages'
 
+// NOTE:
 // convention is that uppercased letters denote encoded letters, lowercased
 // letters denote either the solution text or the user's current guess at the
 // solution text
@@ -24,18 +25,17 @@ const QuoteSolve = ({ quote, user }) => {
 
   // set up individual elements for each letter
   // those individual elements show the current guess above the formatted letter
-  // set event listeners onClick for alphabetical letters
 
-  // onClick checks the formattedText/Author letter of that element and uses
-  // that as the key to update guessedLetters
-
-  // when guessedLetters changes, update guess
+  // set event listeners onClick/onType for alphabetical letters
+  // onClick/onType checks the formattedText/Author letter of that element and uses
+  // that as the key to update guessedLetters (as cipherLetter)
 
   const handleGuess = (cipherLetter, newGuess) => {
     newGuess = newGuess.toLowerCase()
     const oldGuess = guessHash[cipherLetter]
     const oldCipherPair = Object.entries(guessHash).find(pair => pair[1] === newGuess)
     const newGuessQuote = { ...guessQuote }
+    const newGuessHash = { ...guessHash }
     // newGuess was previously guessed elsewhere
     if (oldCipherPair) {
       // newGuess was previously guessed elsewhere & cipherLetter had a previous guess
@@ -44,9 +44,11 @@ const QuoteSolve = ({ quote, user }) => {
         newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${oldGuess}`, 'g'), cipherLetter)
         newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${newGuess}`, 'g'), oldGuess)
         newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${newGuess}`, 'g'), oldGuess)
+        newGuessHash[oldCipherPair[0]] = oldGuess
       } else {
         newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${newGuess}`, 'g'), oldCipherPair[0])
         newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${newGuess}`, 'g'), oldCipherPair[0])
+        newGuessHash[oldCipherPair[0]] = null
       }
       // cipherLetter had a previous guess
     } else if (oldGuess) {
@@ -56,7 +58,8 @@ const QuoteSolve = ({ quote, user }) => {
     // base case
     newGuessQuote.text = newGuessQuote.text.replace(new RegExp(`${cipherLetter}`, 'g'), newGuess)
     newGuessQuote.author = newGuessQuote.author.replace(new RegExp(`${cipherLetter}`, 'g'), newGuess)
-    setGuessHash((new Set(guessHash))[cipherLetter] = newGuess)
+    newGuessHash[cipherLetter] = newGuess
+    setGuessHash(newGuessHash)
     setGuessQuote(newGuessQuote)
   }
 
